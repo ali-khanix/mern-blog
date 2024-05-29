eaf40138c90a11b353b7bc03d8b53cf2b388f88f
Ali Khani <khaniali568@gmail.com>
2024-05-29 07:26:21 +0330

    Just created User

---
+++ server/index.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import mongoose from 'mongoose';
+
+mongoose
+  .connect(
+    'mongodb+srv://ali:ali@mern-blog.mosxda6.mongodb.net/mern-blog?retryWrites=true&w=majority&appName=mern-blog'
+  )
+  .then(console.log('mongoDB is connected'))
+  .catch((err) => {
+    console.log(err);
+  });
+
+const app = express();
+
+app.listen(3000, () => {
+  console.log('Server running on port 3000');
+});
