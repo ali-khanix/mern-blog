da49d69af243bf967c14b0c71349ad51030e4a94
ali_khanix <khaniali568@gmail.com>
2024-06-12 12:55:07 +0330

    create signup api route

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ import express from 'express';
 import mongoose from 'mongoose';
 import userRoutes from './src/routes/user.route.js';
 
+import authRoutes from './src/routes/auth.route.js';
+
 mongoose
   .connect(
     'mongodb+srv://ali:ali@mern-blog.mosxda6.mongodb.net/mern-blog?retryWrites=true&w=majority&appName=mern-blog'
@@ -12,9 +14,11 @@ mongoose
   });
 
 const app = express();
+app.use(express.json());
 
 app.listen(3000, () => {
   console.log('Server running on port 3000!');
 });
 
 app.use('/server/user', userRoutes);
+app.use('/server/auth', authRoutes);
--- server/src/routes/user.route.js
+++ server/src/routes/user.route.js
@@ -1,11 +1,9 @@
 import express from 'express';
+import { test } from '../controllers/user.controller.js';
+test;
 
 const router = express.Router();
 
-router.get('/test', (req, res) => {
-  res.json({
-    message: 'API is working!',
-  });
-});
+router.get('/test', test);
 
 export default router;
